ed0d3124c1bbd3c6dec262eebfb5e6cba66706e3
iimik <iimik@qq.com>
2025-01-10 14:50:44 +0800

    Open Markdown icon and i18n

--- idea-plugin/src/main/kotlin/org/ifinalframework/api/idea/action/OpenMarkdownAction.kt
+++ idea-plugin/src/main/kotlin/org/ifinalframework/api/idea/action/OpenMarkdownAction.kt
@@ -2,11 +2,13 @@ package org.ifinalframework.api.idea.action;
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.itangcent.idea.plugin.actions.BasicAnAction
 import com.itangcent.intellij.context.ActionContext
 import org.ifinalframework.api.idea.markdown.MarkdownOpener
+import javax.swing.Icon
 
 
 /**
@@ -15,7 +17,11 @@ import org.ifinalframework.api.idea.markdown.MarkdownOpener
  * @author iimik
  * @since 2025-01-08 20:42
  **/
-class OpenMarkdownAction : BasicAnAction("Open Markdown") {
+class OpenMarkdownAction : BasicAnAction {
+    constructor() : super()
+    constructor(icon: Icon?) : super(icon)
+    constructor(text: String?) : super(text)
+    constructor(text: String?, description: String?, icon: Icon?) : super(text, description, icon)
     override fun actionPerformed(actionContext: ActionContext, project: Project?, anActionEvent: AnActionEvent) {
         super.actionPerformed(actionContext, project, anActionEvent)
         actionContext.runInReadUI {
@@ -23,4 +29,5 @@ class OpenMarkdownAction : BasicAnAction("Open Markdown") {
             actionContext.instance(MarkdownOpener::class).open(element)
         }
     }
+
 }
